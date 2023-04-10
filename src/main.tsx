c72d259500743a22ffe76a2ee1bdb868563d90bc
Tiago Feitor <tiagoazevedofeitor@gmail.com>
2023-04-10 16:59:34 +0000

    Netlify changes

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,8 @@ import {ScrollToTop}  from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root')! );
 root.render(
-  <React.StrictMode>
     <Router>
       <ScrollToTop/>
       <App />
     </Router>
-  </React.StrictMode>
 );
